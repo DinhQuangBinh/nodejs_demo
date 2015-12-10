070a37bed437b8528bc5b211fea28caa09f30c2c
Kyle Dinh <dinhquangbinh@live.com>
2015-12-10 10:55:02 +0700

    update

--- server/controllers/meetups-controller.js
+++ server/controllers/meetups-controller.js
@@ -1,16 +1,38 @@
 var Meetup = require('../models/meetup');
 
 module.exports.create = function (req, res) {
-  var meetup = new Meetup(req.body);
-  meetup.name = req.body.name;
-  meetup.age = req.body.age;
-  meetup.save(function (err, result) {
-        res.json(result);
-  });
+    try {
+        var meetup = new Meetup(req.body);
+        meetup.name = req.body.name;
+        meetup.age = req.body.age;
+        meetup.save(function (err, results) {
+            //res.json(result);
+            var result  = [];
+            result.push({status: '1000', message: 'OK', data: results});
+            res.contentType('application/json');
+            res.send(JSON.stringify(result));
+        });       
+    } catch (ex) {
+        var result  = [];
+        result.push({status: '1001', message: 'ERROR'});
+        res.contentType('application/json');
+        res.send(JSON.stringify(result));
+    }
 }
 
 module.exports.list = function (req, res) {
-  Meetup.find({}, function (err, results) {
-      res.json(results);
-  });
+    try {
+        Meetup.find({}, function (err, results) {
+            //res.json(results);
+            var result  = [];
+            result.push({status: '1000', message: 'OK', data: results});
+            res.contentType('application/json');
+            res.send(JSON.stringify(result));
+        });       
+    } catch (ex) {
+        var result  = [];
+        result.push({status: '1001', message: 'ERROR'});
+        res.contentType('application/json');
+        res.send(JSON.stringify(result));
+    }
 }
