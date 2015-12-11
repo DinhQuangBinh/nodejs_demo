306c5cc6492ce97f0a12431df0afd1425ea5be76
Kyle Dinh <dinhquangbinh@live.com>
2015-12-11 17:15:20 +0700

    update

--- server/controllers/items-controller.js
+++ server/controllers/items-controller.js
@@ -2,32 +2,70 @@ var Item = require('../models/items');
 var Meetup = require('../models/meetup');
 var mysql = require('mysql');
     
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
       host     : '172.30.1.13',
       user     : 'nahi_dev',
       password : 'nahi_dev123',
-      database : 'nahi_sg_prod'
+      database : 'nodejs_demo',
+      port     : 3307,
+      multipleStatements: true
     });
 
 module.exports.list = function (req, res) {
-    
-    var limit = req.query.limit;
-    console.log(limit);
-    connection.query('SELECT * FROM items limit 0,'+limit,function(err,rows){
-      if(err) throw err;
+    //var limit = req.query.limit;
+    //console.log(limit);
+    pool.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM items limit 0,10',function(err,rows){
+          if(err) throw err;
 
-      console.log('Data received from Db:\n');
-      console.log(rows);
-      res.json(rows);
+          console.log('Data received from Db:\n');
+          //console.log(rows);
+          res.json(rows);
+        });
     });
     
-    connection.connect(function(err){
-        if(!err) {
-            console.log("Mysql db is connected ... \n\n");  
-        } else {
-            console.log("Error connecting Mysql db ... \n\n");  
-        }
-    });
+    /*Item.find({}, function (err, results) {
+      //var sys = req.params.sys;
+      //res.send(sys);
+      res.json(results);
+  });*/
+}
+
+module.exports.create = function (req, res) {
+    var name = req.body.name; 
+    var number = req.body.number;
+    if (typeof number !== 'undefined' && number !== null && number !== '')
+    {
+        pool.getConnection(function(err, connection) {
+            if(err) {
+                console.log(err);
+                res.send(err);
+            } else {
+                var sql = "INSERT INTO items (name) VALUES (?)";
+                var sql_text = "";
+                for(var i = 0; i < number; i++){
+                    sql_text += mysql.format(sql, [name]) + ";";
+                }
+                //sql_text = sql_text.substring(0, sql_text.length - 1);
+                console.log(sql_text);
+                connection.query(sql_text, function(err, rows){
+
+                    //console.log('Data received from Db:\n');
+                    //console.log(rows);
+                    res.json(rows);
+                    //res.end();
+                });
+            }
+
+
+        });
+    }
+    
+    else
+    {
+        res.send('number không được rỗng');
+    }
+
     
     
     
